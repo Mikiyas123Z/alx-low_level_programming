b8b44edb9bb2f9aef81c2a450e0d93807231330e
Mikiyas123Z <mikimanand@gmail.com>
2022-07-01 09:27:36 -0700

    push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - this is the documentation
+ *
+ *   *
+ *
+ *    * Return: a zero value
+ *
+ *     */
+
+
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - this is the documentation
+ *
+ *   *
+ *
+ *    * Return: a zero value
+ *
+ *     */
+
+
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
