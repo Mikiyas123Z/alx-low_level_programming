bd6383334468bb68171617a562b02b8c2d5ac4a0
Mikiyas123Z <mikimanand@gmail.com>
2022-07-03 04:31:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-
-
-/**
- *
- *  * main - this is the documentation
- *
- *   *
- *
- *    * Return: a zero value
- *
- *     */
-
-
+ /**
+ * * main -Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
